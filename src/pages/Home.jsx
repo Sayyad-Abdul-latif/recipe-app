4bfbfe0407dcd65038b5b794699176456c437143
Sayyad-Abdul-latif <sayyadabdullatif0907@gmail.com>
2022-11-09 18:23:38 +0530

    pages uploaded

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Veggie from "../components/Veggie"
+import Popular from "../components/Popular"
+function Home() {
+  return (
+    <div>
+        <Veggie/>
+        <Popular/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/Pages.jsx
@@ -0,0 +1,13 @@
+import Home from "./Home";
+
+import React from 'react'
+
+function Pages() {
+  return (
+    <div>
+        <Home/>
+    </div>
+  )
+}
+
+export default Pages
