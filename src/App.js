6f0929526cb45b6bf2f6b869deb74688b2230ea0
Sayyad-Abdul-latif <sayyadabdullatif0907@gmail.com>
2022-11-18 19:33:52 +0530

    e

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import Search from "./components/Search";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Search />
         <Category />
         <Pages />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
